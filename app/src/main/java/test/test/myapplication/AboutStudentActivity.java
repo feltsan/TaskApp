95e173a1e437ed57e3879212e51af4bf8917de44
Ivan Feltsan <ivan.feltsan@gmail.com>
2014-10-13 21:38:49 +0300

    task1

--- app/src/main/java/test/test/myapplication/AboutStudentActivity.java
+++ app/src/main/java/test/test/myapplication/AboutStudentActivity.java
@@ -1,9 +1,13 @@
 package test.test.myapplication;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,8 +17,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.InputStream;
+import java.util.StringTokenizer;
 
 import test.test.myapplication.supp.ImageLoader;
+import test.test.myapplication.supp.TaskDataBase;
 
 
 public class AboutStudentActivity extends Activity {
@@ -23,9 +29,11 @@ public class AboutStudentActivity extends Activity {
     String image;
     ImageView img;
     ImageLoader imageLoader = new ImageLoader(this);
+    TaskDataBase dbHelper;
+    SQLiteDatabase db;
 
-    TextView tvText;
-
+    TextView tvText, tvFName;
+    Cursor c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +46,31 @@ public class AboutStudentActivity extends Activity {
        // image = intent.getStringExtra("image");
 
 
-        TextView tvFName = (TextView) findViewById(R.id.name);
+        dbHelper = new TaskDataBase(this);
+        db = dbHelper.getWritableDatabase();
+
+        tvFName = (TextView) findViewById(R.id.name);
         tvText = (TextView) findViewById(R.id.text);
         img = (ImageView) findViewById(R.id.imageView);
-        AsyncJSON asyncJSON = new AsyncJSON();
-        asyncJSON.execute();
+
+        new AsyncJSON().execute();
+
+        c = db.query(TaskDataBase.TABLE_NAME,new String[]{TaskDataBase.UID, TaskDataBase.NAME,
+                TaskDataBase.TEXT, TaskDataBase.SMALL_IMAGE, TaskDataBase.BIG_IMAGE },"name = ?", new String[]{name} , null, null, null);
+        c.moveToFirst();
+
+            tvText.setText(c.getString(c.getColumnIndex(TaskDataBase.TEXT)));
+            tvFName.setText(c.getString(c.getColumnIndex(TaskDataBase.NAME)));
+            imageLoader.DisplayImage(c.getString(c.getColumnIndex(TaskDataBase.BIG_IMAGE)), img);
+            c.close();
 
 
-        tvFName.setText(name);
 
-
+//        db.close();
+//        dbHelper.close();
+//
     }
+
     private class AsyncJSON extends AsyncTask<Object, Long, JSONObject>{
         @Override
         protected void onPreExecute(){
@@ -60,13 +82,20 @@ public class AboutStudentActivity extends Activity {
         @Override
     protected JSONObject doInBackground(Object[] params) {
             JSONObject jsonArray = new JSONObject();
+            ContentValues cv = new ContentValues();
             HttpRequest request = HttpRequest.get("http://dev.tapptic.com/test/json.php?name="+name);
             if (request.code() == 200){
                 String response = request.body();
                 try {
                     jsonArray = new JSONObject(response);
                     text = jsonArray.getString("text");
+                    cv.put(ThirdLessonActivity.KEY_TEXT, text);
                     image = jsonArray.getString("image");
+                    cv.put(TaskDataBase.BIG_IMAGE, image);
+                    db.update(TaskDataBase.TABLE_NAME,cv, "name = ?", new String[]{name});
+//                    Log.d("Log", image +" "+ name+" "+ text);
+
+
                 } catch (JSONException e){
                     e.printStackTrace();
                 }
@@ -77,10 +106,9 @@ public class AboutStudentActivity extends Activity {
         }
         protected void onPostExecute(JSONObject  result){
             super.onPostExecute(result);
-            tvText.setText(text);
-            imageLoader.DisplayImage(image, img);
-        }
 
+
+        }
     }
 
  }
--- app/src/main/java/test/test/myapplication/ThirdLessonActivity.java
+++ app/src/main/java/test/test/myapplication/ThirdLessonActivity.java
@@ -41,7 +41,7 @@ public class ThirdLessonActivity extends Activity {
     SQLiteDatabase db;
 
     public static final String KEY_NAME = "name";
-    public static final String KEY_SNAME = "sname";
+    public static final String KEY_TEXT = "text";
     public static final String KEY_IMAGE = "image";
 
     @Override
@@ -49,74 +49,95 @@ public class ThirdLessonActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.listview_main);
-
-        new DownloadJSON().execute();
-
+  //      new DownloadJSON().execute();
         dbHelper = new TaskDataBase(this);
         db = dbHelper.getWritableDatabase();
-
-    }
-
-    private class DownloadJSON extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected void onPreExecute(){
-            super.onPreExecute();
-            mProgressDialog = new ProgressDialog(ThirdLessonActivity.this);
-            mProgressDialog.setMessage("Loading...");
-            mProgressDialog.setIndeterminate(false);
-            mProgressDialog.show();
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            arrayList = new ArrayList<HashMap<String, String>>();
-            jsonArray = JSONFunctions.getJSONfromURL("http://dev.tapptic.com/test/json.php");
-
-            try {
-                    for (int i = 0; i<jsonArray.length(); i++){
-                    HashMap<String, String> map = new HashMap<String, String>();
-                    ContentValues cv = new ContentValues();
-                    jsonObject = jsonArray.getJSONObject(i);
-                    map.put("name", jsonObject.getString("name"));
-                    cv.put(TaskDataBase.NAME, jsonObject.getString("name"));
-                    map.put("image", jsonObject.getString("image"));
-                    cv.put(TaskDataBase.SMALL_IMAGE, jsonObject.getString("image"));
-                    cv.put(TaskDataBase.BIG_IMAGE, jsonObject.getString("image"));
-                        db.insert(TaskDataBase.TABLE_NAME, null, cv);
+        arrayList = new ArrayList<HashMap<String, String>>();
 
 
 
-                        arrayList.add(map);
+        // ContentValues cv = new ContentValues();
 
-                }
+        Cursor c = db.query(TaskDataBase.TABLE_NAME,new String[]{TaskDataBase.UID, TaskDataBase.NAME,
+                TaskDataBase.TEXT, TaskDataBase.SMALL_IMAGE, TaskDataBase.BIG_IMAGE },null, null, null, null, null);
 
-            }catch (JSONException e ){
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-            return null;
-        }
-        protected void onPostExecute(Void args){
-            Cursor c = db.query(TaskDataBase.TABLE_NAME, new String[]{TaskDataBase.UID, TaskDataBase.NAME,
-                    TaskDataBase.TEXT, TaskDataBase.SMALL_IMAGE, TaskDataBase.BIG_IMAGE}, null, null, null, null, null);
-            while (c.moveToNext()) {
+        if (c.moveToFirst()){
 
-                Log.d("myLogs", "ID = " + c.getString(c.getColumnIndex(TaskDataBase.UID)) + ", name = "
-                        + c.getString(c.getColumnIndex(TaskDataBase.NAME)) + ", text = " +
-                        c.getString(c.getColumnIndex(TaskDataBase.TEXT))
-                        + ", s_img = " + c.getString(c.getColumnIndex(TaskDataBase.SMALL_IMAGE)) +
-                        ", b_img = " + c.getString(c.getColumnIndex(TaskDataBase.BIG_IMAGE)));
-            }
+        do{
+
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put("name", c.getString(c.getColumnIndex(TaskDataBase.NAME)));
+            map.put("image", c.getString(c.getColumnIndex(TaskDataBase.SMALL_IMAGE)));
+            arrayList.add(map);
+
+            Log.d("myLogs", "ID = " + c.getString(c.getColumnIndex(TaskDataBase.UID)) + ", name = "
+                    + c.getString(c.getColumnIndex(TaskDataBase.NAME)) + ", text = " +
+                    c.getString(c.getColumnIndex(TaskDataBase.TEXT))
+                    + ", s_img = " + c.getString(c.getColumnIndex(TaskDataBase.SMALL_IMAGE)) +
+                    ", b_img = " + c.getString(c.getColumnIndex(TaskDataBase.BIG_IMAGE)));
+
+        }while (c.moveToNext());
+
+        }else
             c.close();
 
-            listView = (ListView) findViewById(R.id.listview);
-            adapter = new ListViewAdapter(ThirdLessonActivity.this, arrayList);
-            listView.setAdapter(adapter);
-            mProgressDialog.dismiss();
+        listView = (ListView) findViewById(R.id.listview);
+        adapter = new ListViewAdapter(ThirdLessonActivity.this, arrayList);
+        listView.setAdapter(adapter);
+    //    db.delete("task_table", null, null);
+      //  dbHelper.close();
+      //  db.close();
 
-        }
 
+
+//     mProgressDialog.dismiss();
     }
+
+//    private class DownloadJSON extends AsyncTask<Void, Void, Void> {
+//
+//        @Override
+//        protected void onPreExecute(){
+//            super.onPreExecute();
+//            mProgressDialog = new ProgressDialog(ThirdLessonActivity.this);
+//            mProgressDialog.setMessage("Loading...");
+//            mProgressDialog.setIndeterminate(false);
+//            mProgressDialog.show();
+//        }
+//
+//        @Override
+//        protected Void doInBackground(Void... params) {
+//            arrayList = new ArrayList<HashMap<String, String>>();
+//            jsonArray = JSONFunctions.getJSONfromURL("http://dev.tapptic.com/test/json.php");
+//
+//            try {
+//                    for (int i = 0; i<jsonArray.length(); i++){
+//                    //HashMap<String, String> map = new HashMap<String, String>();
+//                    ContentValues cv = new ContentValues();
+//                    jsonObject = jsonArray.getJSONObject(i);
+//                   // map.put("name", jsonObject.getString("name"));
+//                    cv.put(TaskDataBase.NAME, jsonObject.getString("name"));
+//                   // map.put("image", jsonObject.getString("image"));
+//                    cv.put(TaskDataBase.SMALL_IMAGE, jsonObject.getString("image"));
+//                   // cv.put(TaskDataBase.BIG_IMAGE, jsonObject.getString("image"));
+//                        db.insert(TaskDataBase.TABLE_NAME, null, cv);
+//
+//
+//
+//                     //   arrayList.add(map);
+//
+//                }
+//
+//            }catch (JSONException e ){
+//                Log.e("Error", e.getMessage());
+//                e.printStackTrace();
+//            }
+//            return null;
+//        }
+//        protected void onPostExecute(Void args){
+//
+//
+//        }
+//
+//}
 
 }
